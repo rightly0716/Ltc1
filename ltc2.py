c5a3f729696e302a272d20776120152721a1a58b
rightly0716 <jingxiangchen0716@gmail.com>
2018-08-12 20:31:03 -0700

    N queen

--- ltc2.py
+++ ltc2.py
@@ -4357,10 +4357,14 @@ There exist two distinct solutions to the 4-queens puzzle:
   "...Q",
   ".Q.."]
 ]
+经典的N皇后问题，基本所有的算法书中都会包含的问题，经典解法为回溯递归，一层一层的向下扫描，需要用到一个pos数组，
+其中pos[i]表示第i行皇后的位置，初始化为-1，然后从第0开始递归，每一行都一次遍历各列，判断如果在该位置放置皇后会不会有冲突，
+以此类推，当到最后一行的皇后放好后，一种解法就生成了，将其存入结果res中，然后再还会继续完成搜索所有的情况，代码如下：
 """
 from copy import deepcopy
-n = 4
-n = 8
+n = 4 # 2
+n = 8 # 92
+n = 10 # 742
 all_board = solveNQueens(n)
 len(all_board)
 
