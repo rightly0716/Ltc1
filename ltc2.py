30b105a47ed876e70bda63d48d85de88e322eac3
Sean <rightly0716@gmail.com>
2019-09-04 21:35:39 -0700

    add new q
    add new

--- ltc2.py
+++ ltc2.py
@@ -867,40 +867,6 @@ areSentencesSimilarTwo(words1, words2, pairs)
 
 words1 = ["great"]; words2 = ["great"]; pairs = []
 
-"""Accounts Merge 账户合并 
-Given a list accounts, each element accounts[i] is a list of strings, where the
- first element accounts[i][0] is a name, and the rest of the elements are emails
- representing emails of the account.
-Now, we would like to merge these accounts. Two accounts definitely belong to
- the same person if there is some email that is common to both accounts. Note
- that even if two accounts have the same name, they may belong to different
- people as people could have the same name. A person can have any number of accounts
- initially, but all of their accounts definitely have the same name.
-After merging the accounts, return the accounts in the following format: the
- first element of each account is the name, and the rest of the elements are
- emails in sorted order. The accounts themselves can be returned in any order.
-
-Example 1:
-Input: 
-accounts = [["John", "johnsmith@mail.com", "john00@mail.com"], ["John", "johnnybravo@mail.com"], 
- ["John", "johnsmith@mail.com", "john_newyork@mail.com"], ["Mary", "mary@mail.com"]]
-Output: [["John", 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com'],  
- ["John", "johnnybravo@mail.com"], ["Mary", "mary@mail.com"]]
-Explanation: 
-The first and third John's are the same person as they have the common email 
-"johnsmith@mail.com".
-The second John and Mary are different people as none of their email addresses 
-are used by other accounts.
-We could return these lists in any order, for example the answer 
-[['Mary', 'mary@mail.com'], ['John', 'johnnybravo@mail.com'], 
-['John', 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com']] would 
-still be accepted.
-"""
-accounts = [["John", "johnsmith@mail.com", "john00@mail.com"], ["John", "johnnybravo@mail.com"], 
- ["John", "johnsmith@mail.com", "john_newyork@mail.com"], ["Mary", "mary@mail.com"]]
-
-def accountsMerge(accounts):
-    return None
     
 """Two Sum III - Data structure design 两数之和之三 - 数据结构设计 
 Design and implement a TwoSum class. It should support the following operations:add and find.
