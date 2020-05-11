ce83319b72b5fa5e80f1ac5596b90e5f4a9590b8
Sean Chen <jxchen@amazon.com>
2020-05-11 13:29:10 -0700

    Update ltc3.py

--- ltc3.py
+++ ltc3.py
@@ -125,4 +125,120 @@ def copytree(node):
     return head
 
 
+"""
+68. Text Justification
+Given an array of words and a width maxWidth, format the text such that each line has exactly maxWidth characters and is fully (left and right) justified.
+
+You should pack your words in a greedy approach; that is, pack as many words as you can in each line. Pad extra spaces ' ' when necessary so that each line has exactly maxWidth characters.
+
+Extra spaces between words should be distributed as evenly as possible. If the number of spaces on a line do not divide evenly between words, the empty slots on the left will be assigned more spaces than the slots on the right.
+
+For the last line of text, it should be left justified and no extra space is inserted between words.
+
+Note:
+
+A word is defined as a character sequence consisting of non-space characters only.
+Each word's length is guaranteed to be greater than 0 and not exceed maxWidth.
+The input array words contains at least one word.
+Example 1:
+
+Input:
+words = ["This", "is", "an", "example", "of", "text", "justification."]
+maxWidth = 16
+Output:
+[
+   "This    is    an",
+   "example  of text",
+   "justification.  "
+]
+
+Example 2:
+
+Input:
+words = ["What","must","be","acknowledgment","shall","be"]
+maxWidth = 16
+Output:
+[
+  "What   must   be",
+  "acknowledgment  ",
+  "shall be        "
+]
+Explanation: Note that the last line is "shall be    " instead of "shall     be",
+             because the last line must be left-justified instead of fully-justified.
+             Note that the second line is also left-justified becase it contains only one word.
+
+Example 3:
+
+Input:
+words = ["Science","is","what","we","understand","well","enough","to","explain",
+         "to","a","computer.","Art","is","everything","else","we","do"]
+maxWidth = 20
+Output:
+[
+  "Science  is  what we",
+  "understand      well",
+  "enough to explain to",
+  "a  computer.  Art is",
+  "everything  else  we",
+  "do                  "
+]
+"""
+
+def text_justification(words, maxwidth):
+  cum_len = 0
+  output = []
+  curr_words = []
+  rest_words = words
+  while rest_words:
+      curr_line, rest_words = extract_line(rest_words, maxwidth)
+      output.append(curr_line)
+
+  return output
+
+def extract_line():
+  # get evenly distributed word line
+
+  return 
+
+
+
+
+# Given an array of integers and an integer k, 
+# you need to find the total number of continuous subarrays whose sum equals to k.
+
+# Example 1:
+# Input:nums = [1,1,1], k = 2
+
+# Output: 2
+def num_subarrays(nums, k):
+  # first cumsum 
+  res = 0
+  cumsum_arr, ht1 = cumsum(nums)
+  for i in cumsum_arr:
+    if i == k:
+      res = res + 1
+    if k-i in ht1:
+      res = res + len(ht1[k-i])
+
+  return res
+
+
+def cumsum(nums):
+  cumsum_arr = []
+  sum_so_far = 0
+  ht = {}
+  for i in range(len(nums)):
+    sum_so_far = sum_so_far + nums[i]
+    cumsum_arr.append(sum_so_far)
+    if sum_so_far not in ht:
+      ht[sum_so_far] = 1
+    else:
+      ht[sum_so_far]+=1
+  return cumsum_arr, hs
+
+
+
+
+
+
 
